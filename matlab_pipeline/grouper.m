52f840f468b2da6ba6819f6745a49c288ac59be6
shivChitinous <shivamchitnis66@gmail.com>
2020-01-08 12:50:25 +0530

    pipeline and data files

--- matlab_pipeline/grouper.m
+++
@@ -1,9 +0,0 @@
-function [groups,fNT] = grouper(NT, T)
-    x = confusionmat(T, NT);
-    [~,idx] = max(x);
-    groups = idx;
-    fNT = NT;
-    for i = 1:size(groups,2)
-        fNT(NT==i) = groups(:,i);
-    end          
-end
