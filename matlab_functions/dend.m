114afee973c30740092c48589ef5c10f1e502c14
shivChitinous <shivamchitnis66@gmail.com>
2020-01-17 02:45:17 +0530

    -

--- matlab_functions/dend.m
+++
@@ -1,4 +0,0 @@
-function dend(data)
-    z = linkage(data,'ward');
-    dendrogram(z);
-end
--- matlab_functions/pcacorr.m
+++
@@ -1,7 +0,0 @@
-function [coeff, score, latent, expl] = pcacorr(n)
-    V = cov(n);
-    SD = sqrt(diag(V));
-    R = V./(SD*SD');
-    [coeff, latent, expl] = pcacov(R);
-    score = (zscore(n))*coeff;
-end
