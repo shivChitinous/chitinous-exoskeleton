d0b0e5262d9c99e012c1aab9069529602db1fe23
shivChitinous <shivamchitnis66@gmail.com>
2020-01-17 02:35:44 +0530

    mlx_pipeline

--- cluster_data.m
+++
@@ -1,4 +0,0 @@
-function T = cluster_data(data,n)
-    z = linkage(data,'ward','euclidean');
-    T = cluster(z,'maxclust',n);
-end
--- song.m
+++
@@ -1,9 +0,0 @@
-function [songdata, songP, songNT] = song(rawdata, SC, GL, P, NT)
-    nicedata = rawdata(ismember(GL,'Y')&(~(any(isnan(rawdata),2))),:);
-    niceSC = SC(ismember(GL,'Y')&(~(any(isnan(rawdata),2))),:);
-    niceP = P(ismember(GL,'Y')&(~(any(isnan(rawdata),2))),:);
-    niceNT = NT(ismember(GL,'Y')&(~(any(isnan(rawdata),2))),:);
-    songdata = nicedata(ismember(niceSC,'S'),:);
-    songP = niceP(ismember(niceSC,'S'),:);
-    songNT = niceNT(ismember(niceSC,'S'),:);
-end
